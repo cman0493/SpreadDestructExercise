3f4bc86ae361dd6bc18079d43f608990b0ae779f
Chris Hunley <cshunley21@gmail.com>
2021-10-21 09:01:38 -0500

    Fixed bonus

--- script.js
+++ script.js
@@ -22,20 +22,24 @@ function seeCharacters ({ellieSattler}) {
 seeCharacters(jPCharacters)
 
 // 3
-const jurassicParkMovies = {
-    one: `Jurassic Park`,
-    two: `The Lost World: Jurassic Park`,
-    three: `Jurassic Park III`,
-    four: `Jurassic World`, 
-    five: `Jurassic World: Fallen Kingdom`,
-    six: `Jurassic World: Dominion`
-};
-function seeJPMovies ({one, two, three, four, five, six}) {
+const jurassicParkMovies = [
+    {
+      one: "Jurassic Park",
+      two: "The Lost World: Jurassic Park",
+      three: "Jurassic Park III"
+    },
+    {
+      four: "Jurassic World",
+      five: "Jurassic World: Fallen Kingdom",
+      six: "Jurassic World: Dominion"
+    }
+  ];
+  seeJPMovies = ([{one, two, three}, {four, five, six}]) => {
     console.log(one);
-    console.log(two);
-    console.log(three);
-    console.log(four);
-    console.log(five);
-    console.log(six);
-}
-seeJPMovies(jurassicParkMovies)
+   console.log(two);
+   console.log(three);
+   console.log(four);
+   console.log(five);
+   console.log(six);
+  }
+  seeJPMovies(jurassicParkMovies);
